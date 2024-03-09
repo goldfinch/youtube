51e691edc0c84cbbe277d9b0b93b7cba89f0a41e
Art Karasev <goldfinch@users.noreply.github.com>
2024-03-10 03:08:45 +1300

    WIP

--- src/Blocks/YoutubeBlock.php
+++ src/Blocks/YoutubeBlock.php
@@ -29,9 +29,9 @@ class YoutubeBlock extends BaseElement
 
     public function getCMSFields()
     {
-        $fields = parent::getCMSFields();
+        $fields = parent::getCMSFields()->initFielder($this);
 
-        $fielder = $this->intFielder($fields)->getFielder();
+        $fielder = $fields->getFielder();
 
         if ($fielder && $fielder->datafield('VideoLimit')) {
             $fielder
